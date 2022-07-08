5dc203a382305bfec2cda04ff4a1b8aebe9d7c4d
GOJAx64 <josealbertogarcia453@gmail.com>
2022-07-08 15:42:46 -0500

    Git ignore

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <h1 className=''>PortfolioApp</h1>
+    <>
+      <h1 className=''>PortfolioApp</h1>
+      <h1>Prueba</h1>
+    </>
   )
 }
