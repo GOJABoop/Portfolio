616cf5addc2ad7d0a661ad47125c9472a93ba0aa
GOJAx64 <josealbertogarcia453@gmail.com>
2022-07-08 15:36:53 -0500

    Changin to React App

--- public/app.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector('#menuButton');
-const menu = document.querySelector('#menu');
-
-menuButton.addEventListener('click', () =>{
-    menu.classList.toggle('hidden')
-})
